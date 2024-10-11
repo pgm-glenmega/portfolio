bb49fac35df6b004ca43a76c966b54bad0839697
pgm-glenmega <glenmega@student.arteveldehs.be>
2024-10-11 18:25:33 +0200

    1 app.js for file

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,7 @@ import fadeAnimation from "./animations/fadeupanimation.js";
 import arrowAnimation from "./animations/arrowanimation.js";
 import heroTitleAnimation from "./animations/banneranimation.js";
 import mouseMove from "./animations/mouseanimation.js";
+import interactAnimation from "./animations/interactanimation.js";
 
 //class functions
 import headerBackground from "./functions/headerBackground.js";
@@ -13,6 +14,7 @@ const executeAnimations = () => {
   headerBackground();
   heroTitleAnimation();
   moveUpTitle();
+  interactAnimation();
   fadeAnimation();
   moveUpTitleRight();
   arrowAnimation();
